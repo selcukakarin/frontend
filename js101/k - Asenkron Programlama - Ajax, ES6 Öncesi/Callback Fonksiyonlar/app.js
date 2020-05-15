70e2cf2e172da8a4a89d24af3e770caf844fc82d
Selçuk Akarın <selcuk.akarin@hrglob.com>
2020-05-15 20:05:48 +0300

    callback is updated

--- js101/k - Asenkron Programlama - Ajax, ES6 Öncesi/Callback Fonksiyonlar/app.js
+++ js101/k - Asenkron Programlama - Ajax, ES6 Öncesi/Callback Fonksiyonlar/app.js
@@ -1,48 +1,60 @@
 // Callback Fonksiyonlar
 
-langs.forEach(function(lang){
-    console.log(lang);
-});
+// langs.forEach(function(lang){
+//     console.log(lang);
+// });
 
 document.getElementById("btn").addEventListener("click",function(){
     console.log("Tıkla");
 
 });
-function process1(callback){
-    setTimeout(function(){
+function process1(callback,cb2){
+
         console.log("Process 1");
+        callback(cb2);
+        var a;
+        for (i=0;i<100000;i++){
+            a=i+100
+        }
+
+
+}
+function process2(callback){
+   
+        console.log("Process 2");
         callback();
 
-    },3000);
-}
-function process2(){
-    setTimeout(function(){
-        console.log("Process 2");
-    },2000);
     
 }
 
-process1(process2);
+function process3(){
 
-const langs = ["Python","Java","C++"];
+        console.log("Process 3");
 
-function addLang(lang,callback){
-
-    setTimeout(function(){
-        langs.push(lang);
-        console.log("Eklendi");
-        callback();
-    },2000);
-}
-function getAllLangs(){
-    setTimeout(function(){
-        langs.forEach(function(lang){
-            console.log(lang);
-        });
-    },1000);
+    
 }
 
-addLang("Javascript",getAllLangs);
+process1(process2,process3);
+
+// const langs = ["Python","Java","C++"];
+
+// function addLang(lang,callback){
+
+//     setTimeout(function(){
+//         langs.push(lang);
+//         console.log("Eklendi");
+//         callback();
+//     },2000);
+// }
+// function getAllLangs(){
+//     setTimeout(function(){
+//         langs.forEach(function(lang){
+//             console.log(lang);
+//         });
+//     },1000);
+// }
+
+// addLang("Javascript",getAllLangs);
 
 
 
